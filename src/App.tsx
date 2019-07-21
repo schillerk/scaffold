94d2cb499604008570d4a8ce90a50506e456450e
kyle_schiller <kyle_schiller@kyleschllersmbp.airgc.net>
2019-07-21 16:55:10 -0700

    demo Context API and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import * as React from "react";
+import { createContext, useState, useReducer } from "react";
 import Header from "./Components/Header";
 
-export class App extends React.Component {
-  render() {
-    return <Header title="Hello World" />;
+export const StateContext = createContext({
+  counter: 0
+});
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+
+    case "decrement":
+      return { count: state.count - 1 };
+
+    default:
+      return state;
   }
-}
+};
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <StateContext.Provider value={{ counter: 0 }}>
+      <Header title="Hello World" />
+      <div>{state.count}</div>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+    </StateContext.Provider>
+  );
+};
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import { StateContext } from "../App";
 
 type HeaderProps = {
   title: string;
 };
 
 export default function Header({ title }: HeaderProps) {
-  return <div>{title}</div>;
+  return (
+    <StateContext.Consumer>
+      {value => <div>{value.counter}</div>}
+    </StateContext.Consumer>
+  );
 }
