f97f0456b69809b1643c1441d6785d089abcd533
kyle_schiller <kyle_schiller@kyle-schillers-MacBook-Pro.local>
2019-07-22 11:33:28 -0700

    allow specific Spacing props

--- src/Components/shared/Spacing.tsx
+++ src/Components/shared/Spacing.tsx
@@ -1,14 +1,39 @@
 import * as React from "react";
 import { withStyles, withStylesProps } from "Core/withStyles";
 
+const parseSpacing = (unit: number, val: number) => `${val * unit || 0}px`;
+
 type Props = {
-  all: number;
+  all?: number;
+  sides?: number;
+  ends?: number;
+  top?: number;
+  right?: number;
+  bottom?: number;
+  left?: number;
 } & withStylesProps;
 
-function Spacing({ children, theme, all = 0 }: Props) {
-  return <div style={{ margin: theme.ui.unit * all }}>{children}</div>;
+function Spacing({
+  children,
+  all,
+  sides,
+  ends,
+  top,
+  right,
+  bottom,
+  left,
+  theme
+}: Props) {
+  const spacing = `
+    ${parseSpacing(theme.ui.unit, [top, ends, all].find(x => x))}
+    ${parseSpacing(theme.ui.unit, [right, sides, all].find(x => x))}
+    ${parseSpacing(theme.ui.unit, [bottom, ends, all].find(x => x))}
+    ${parseSpacing(theme.ui.unit, [left, sides, all].find(x => x))}
+  `;
+
+  return <div style={{ margin: spacing }}>{children}</div>;
 }
 
-export default withStyles(() => ({}), {
+export default withStyles(({}) => ({}), {
   passThemeProp: true
 })(Spacing);
