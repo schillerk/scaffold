d86af3e78fe38634cc96570d8bbbe139d78de558
kyle_schiller <kyle_schiller@kyleschllersmbp.airgc.net>
2019-07-21 19:04:31 -0700

    absolute import for withStyles

---
+++ src/core/withStyles.js
@@ -0,0 +1,24 @@
+import ThemedStyleSheet from "react-with-styles/lib/ThemedStyleSheet";
+import aphroditeInterface from "react-with-styles-interface-aphrodite";
+import {
+  css,
+  withStyles,
+  ThemeProvider,
+  withStylesProps
+} from "react-with-styles";
+
+const theme = {
+  color: {
+    primary: "#FF5A5F",
+    secondary: "#00A699"
+  },
+  ui: {
+    unit: 8,
+    borderRadius: 3
+  }
+};
+
+ThemedStyleSheet.registerTheme(theme);
+ThemedStyleSheet.registerInterface(aphroditeInterface);
+
+export { css, withStyles, withStylesProps, ThemeProvider, ThemedStyleSheet };
